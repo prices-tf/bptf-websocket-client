ecab1090585ec96b3e171070a96eec2e6204a659
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-06-23 12:33:47 +0200

    feat: add event count logging
    Log the amount of events made in the last second

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,16 +1,25 @@
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { WebsocketService } from '../websocket/websocket.service';
 
 @Injectable()
-export class EventService implements OnModuleDestroy {
+export class EventService implements OnModuleInit, OnModuleDestroy {
+  private logger = new Logger(EventService.name);
   private listener;
+  private events: number;
+  private interval: NodeJS.Timer;
 
   constructor(
     private readonly amqpConnection: AmqpConnection,
     private readonly websocketService: WebsocketService,
   ) {
     this.listener = (event) => {
+      this.events++;
       const data = JSON.parse(event.data);
       this.amqpConnection.publish('bptf-event.created', data.event, data);
     };
@@ -18,7 +27,15 @@ export class EventService implements OnModuleDestroy {
     this.websocketService.listen(this.listener);
   }
 
+  onModuleInit() {
+    this.interval = setInterval(() => {
+      this.logger.verbose('Events: ' + this.events);
+      this.events = 0;
+    }, 1000);
+  }
+
   onModuleDestroy() {
+    clearInterval(this.interval);
     this.websocketService.ignore(this.listener);
   }
 }
